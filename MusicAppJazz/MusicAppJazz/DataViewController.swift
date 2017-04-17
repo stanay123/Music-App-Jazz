2db5f6b48b804e74a377671abb4fb4934d9c2bc0
Tanay Agrawal Srivastava <s_tanay@yahoo.com>
2017-04-17 08:41:15 -0400

    Music App Created

---
+++ MusicAppJazz/MusicAppJazz/DataViewController.swift
@@ -0,0 +1,34 @@
+//
+//  DataViewController.swift
+//  MusicAppJazz
+//
+//  Created by Rachana Srivastava on 4/17/17.
+//  Copyright © 2017 Tanay Srivastava. All rights reserved.
+//
+
+import UIKit
+
+class DataViewController: UIViewController {
+
+    @IBOutlet weak var dataLabel: UILabel!
+    var dataObject: String = ""
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.dataLabel!.text = dataObject
+    }
+
+
+}
+
